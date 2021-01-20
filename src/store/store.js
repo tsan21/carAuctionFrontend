e5c7155cde8874223e5e90baaccfbafd33ed136d
tsan <tsanyip@hotmail.com>
2021-01-20 12:35:12 +0100

    changes in store

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,7 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-var baseUrl = "http://localhost:8095/"
+var baseUrl = "https://tsan-auction.herokuapp.com//"
 
 export const store = new Vuex.Store({
     state: {
